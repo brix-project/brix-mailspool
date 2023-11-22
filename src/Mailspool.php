800ccdd0e7887f8cd6068bb00ee540acbe75a7f0
Matthias Leuffen <matthes@leuffen.de>
2023-11-22 21:53:25 +0100

    update

--- src/Mailspool.php
+++ src/Mailspool.php
@@ -5,6 +5,7 @@ namespace Brix\MailSpool;
 use Brix\Core\AbstractBrixCommand;
 use Lack\Keystore\KeyStore;
 use Lack\MailSpool\Driver\PhpmailerDriver;
+use Phore\Cli\Exception\CliException;
 use Phore\Cli\Output\Out;
 
 class Mailspool extends AbstractBrixCommand
@@ -17,17 +18,25 @@ class Mailspool extends AbstractBrixCommand
         $this->facet = MailSpoolFacet::getInstance();
     }
 
-    public function send(array $argv = [], bool $all = false) {
+    public function send(array $argv = [], bool $all = false, string $delete = "yes") {
         $mails = $this->facet->mailSpooler->list();
+        if (count($argv) === 0)
+            $all = true;
+
         if ($all === false) {
-            $mails = [$mails[((int)$argv[0])-1]];
+            $mailNo = ((int)$argv[0])-1;
+            if ( ! isset($mails[$mailNo]))
+                 throw new CliException("Mail No '$mailNo' not found - provide mail number from list.");
+            $mails = [$mails[$mailNo]];
+
         }
         $sC = $this->facet->config->smtp;
         $this->facet->mailSpooler->setDriver(new PhpmailerDriver($sC->host, $sC->port, $sC->username, KeyStore::Get()->getAccessKey($sC->host), $sC->sender, $sC->sender_name));
         foreach ($mails as $mail) {
-            echo "\nSending mail: " . $mail->getMailSpoolId() . "\n";
-            $this->facet->mailSpooler->send($mail);
+            echo "\nSending mail: " . $mail->getMailSpoolId() . " (Delete: $delete)\n";
+            $this->facet->mailSpooler->send($mail, null, $delete === "yes");
             echo "[OK]\n";
+
         }
 
     }
