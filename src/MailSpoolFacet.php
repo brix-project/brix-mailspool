3192fa6aefb4a8add83b206c863922fc3871691b
Matthias Leuffen <matthes@leuffen.de>
2023-11-23 13:42:15 +0100

    update

--- src/MailSpoolFacet.php
+++ src/MailSpoolFacet.php
@@ -6,6 +6,8 @@ use Brix\Core\Type\BrixEnv;
 use Brix\Mailer\Type\T_MailerConfig;
 use Brix\MailSpool\Type\T_MailSpoolConfig;
 use http\Exception\UnexpectedValueException;
+use Lack\Keystore\KeyStore;
+use Lack\MailSpool\Driver\PhpmailerDriver;
 use Lack\MailSpool\MailSpooler;
 use Lack\MailSpool\OutgoingMail;
 
@@ -31,20 +33,47 @@ class MailSpoolFacet
     }
 
 
-
-
-    public function spoolMail(OutgoingMail $outgoingMail) {
+    /**
+     * Return the mail spool id
+     * 
+     * @param OutgoingMail $outgoingMail
+     * @return string
+     */
+    public function spoolMail(OutgoingMail $outgoingMail) : string {
         $this->mailSpooler->spoolMail($outgoingMail);
+        return $outgoingMail->getMailSpoolId();
     }
 
 
-
-    public function spoolMailFromTemplate(string $template, array $data) {
+    /**
+     * Returns the mailSpoolId
+     * 
+     * @param string $template
+     * @param array $data
+     * @return string
+     * @throws \Exception
+     */
+    public function spoolMailFromTemplate(string $template, array $data) : string {
         $mail = OutgoingMail::FromTemplate($template, $data);
         $this->spoolMail($mail);
+        return $mail->getMailSpoolId();
     }
 
 
+
+    public function sendMail(string $mailId = null, bool $delete = true) {
+        $sC = $this->config->smtp;
+        $this->mailSpooler->setDriver(new PhpmailerDriver($sC->host, $sC->port, $sC->username, KeyStore::Get()->getAccessKey($sC->host), $sC->sender, $sC->sender_name));
+        $mails = $this->mailSpooler->list();
+        foreach ($mails as $mail) {
+            if ($mailId !== null && $mail->getMailSpoolId() !== $mailId)
+                continue;
+            echo "\nSending mail: " . $mail->getMailSpoolId() . " (Delete: $delete)... ";
+            $this->mailSpooler->send($mail, null, $delete);
+            echo "[OK]\n";
+        }
+    }
+    
 
 
     private static $instance;
